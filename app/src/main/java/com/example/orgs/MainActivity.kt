dd08e6626c43f151d848016634e17801b1b27de2
gabrielbmmaia <gbmmaia.contato@gmail.com>
2022-09-10 15:09:23 -0300

    Initial Commit

---
+++ app/src/main/java/com/example/orgs/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.orgs
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity: Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
